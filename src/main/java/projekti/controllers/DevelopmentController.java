92a405d28e0d928b44088d74fb69f6878a09989b
Tuomas Välimäki <t.valimaki@gmail.com>
2020-11-01 14:40:42 +0200

    fix some stuff

--- src/main/java/projekti/controllers/DevelopmentController.java
+++ src/main/java/projekti/controllers/DevelopmentController.java
@@ -5,59 +5,30 @@
  */
 package projekti.controllers;
 
-import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.ResponseBody;
-import projekti.models.IMyQuery;
-import projekti.models.UserAccount;
-import projekti.repositories.CustomQueryRepo;
-import projekti.repositories.UserAccountRepository;
 import projekti.services.ContactsService;
 
 /**
- *
- * @author tvali
+ *  Just some endpoints to manipulate data
+ *  while developing
  */
 @Controller
-@Profile({"production", "dev"})
+@Profile({"dev"})
 public class DevelopmentController {
 
-    @Autowired
-    private UserAccountRepository userAccountRepo;
     
     @Autowired
-    private ContactsService contactsService;
-    
-    @Autowired
-    private CustomQueryRepo custom;
-
-    /*@GetMapping("/dev/createcontact/{targetId}/{contactId}")
-    public String createContact(@PathVariable Long targetId, @PathVariable Long contactId) {
-        contactsService.createContact(targetId, contactId);        
-        return "redirect:/home";
-    }*/
+    private ContactsService contactsService;    
     
     @GetMapping("/dev/deletecontact/{targetId}/{contactId}")
     public String deleteContact(@PathVariable Long targetId, @PathVariable Long contactId) {
-        contactsService.removeContact(targetId, contactId); 
-        //contactsService.removeContact(contactId, targetId);
+        contactsService.removeContact(targetId, contactId);       
         return "redirect:/home";
-    }
-    
-    @GetMapping("/dev/test/{id}")
-    @ResponseBody
-    public String test(@PathVariable Long id){
-        List<IMyQuery> res = custom.getWithFriends(id);
-        
-        for( IMyQuery item : res){
-            System.out.println("" + item.getUsername() + " " + item.getId() + " " + item.getContactsId());
-        }
-        return "redirect:/";
-    }   
+    }  
     
     
     
